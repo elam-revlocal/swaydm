2e2f194aab24a6ae35e75d7dcfe9e0cdb2532df4
Erica Lam <elam@revlocal.com>
2021-10-05 16:38:58 -0400

    update form for checkboxes

--- src/components/Form/CheckBoxLabel.js
+++ src/components/Form/CheckBoxLabel.js
@@ -4,10 +4,38 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 export default function CheckboxLabel() {
+    const [state, setState] = React.useState({
+    checkedBetaTester: false,
+    checkedAmbassador: false,
+  })
+
+  const handleChange = (event) => {
+    setState({ ...state, 
+      [event.target.name]: event.target.value });
+  }
+
+  let {checkedBetaTester, checkedAmbassador} = state;
+  
   return (
     <FormGroup >
-      <FormControlLabel control={<Checkbox />} label="I'm interested in being a beta tester" />
-      <FormControlLabel control={<Checkbox />} label="I'm interested in being an ambassador" />
-    </FormGroup>
+    <FormControlLabel 
+      control=
+        {<Checkbox
+            checked={checkedBetaTester}
+            onChange={handleChange}
+            name="betaTester" />} 
+      name="betaTester" 
+      value="betaTester"  
+      label="I'm interested in being a beta tester" />
+    <FormControlLabel  
+      control=
+        {<Checkbox
+          checked={checkedAmbassador}
+          onChange={handleChange} 
+        />} 
+      name="ambassador" 
+      value="ambassador"  
+      label="I'm interested in being an ambassador" />
+  </FormGroup>   
   );
 }
